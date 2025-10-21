526dfe92a6d9433c1be4379a96d3d2db620525be
Michał Czyż <mike@c2yz.com>
2025-10-21 14:07:12 +0200

    fix: bind

--- nixos/modules/bind.nix
+++ nixos/modules/bind.nix
@@ -12,13 +12,11 @@
       };
     };
 
-    # internal view for WireGuard clients only
     views = {
       internal = {
-        # match your WireGuard subnet (and localhost so server can query)
         matchClients = ["192.168.200.0/24" "127.0.0.1" "::1/128"];
-        recursion = true;
         allowQuery = ["192.168.200.0/24" "127.0.0.1" "::1/128"];
+        recursion = true;
 
         zones = {
           "photos.c2yz.com" = {
@@ -34,7 +32,7 @@
               )
               @ IN NS ns1.c2yz.com.
               ns1 IN A 192.168.200.1
-              photos IN A 192.168.200.10
+              @ IN A 192.168.200.1
             '';
           };
         };
