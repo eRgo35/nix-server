10a2cd9e0a8457bd905405c48bb3fb833ca0a209
Michał Czyż <mike@c2yz.com>
2025-10-21 14:24:47 +0200

    fix: bind

--- nixos/modules/bind.nix
+++ nixos/modules/bind.nix
@@ -1,41 +1,49 @@
 {pkgs, ...}: {
   services.bind = {
     enable = true;
-    cacheNetworks = ["127.0.0.0/8" "::1/128" "192.168.0.0/24"];
+
+    # Limit recursive queries (cache access) to local/internal networks
+    cacheNetworks = [
+      "127.0.0.0/8"
+      "::1/128"
+      "192.168.200.0/24"
+      "192.168.0.0/24"
+    ];
+
     zones = {
-      "c2yz.com" = {
+      # "c2yz.com" = {
+      #   master = true;
+      #   allowQuery = [
+      #     "127.0.0.0/8"
+      #     "::1/128"
+      #     "192.168.0.0/24"
+      #     "192.168.200.0/24"
+      #   ];
+      #   file = pkgs.writeText "zone-c2yz.com" ''
+      #     ; TODO: add your c2yz.com zone records here
+      #   '';
+      # };
+
+      "photos.c2yz.com" = {
         master = true;
-        allowQuery = ["127.0.0.0/8" "::1/128" "192.168.0.0/24"];
-        file = pkgs.writeText "zone-example.com" ''
-          ...
+        allowQuery = [
+          "127.0.0.1"
+          "::1/128"
+          "192.168.200.0/24"
+        ];
+        file = pkgs.writeText "zone-photos.c2yz.com" ''
+          $TTL 300
+          @   IN SOA ns1.c2yz.com. admin.c2yz.com. (
+                    2025102101 ; serial (update)
+                    3600       ; refresh
+                    600        ; retry
+                    604800     ; expire
+                    86400      ; minimum
+          )
+          @   IN NS ns1.c2yz.com.
+          ns1 IN A 192.168.200.1
+          @   IN A 192.168.200.1
         '';
-      };
-    };
-
-    views = {
-      internal = {
-        matchClients = ["192.168.200.0/24" "127.0.0.1" "::1/128"];
-        allowQuery = ["192.168.200.0/24" "127.0.0.1" "::1/128"];
-        recursion = true;
-
-        zones = {
-          "photos.c2yz.com" = {
-            type = "master";
-            file = pkgs.writeText "zone-photos.c2yz.com" ''
-              $TTL 300
-              @   IN SOA ns1.c2yz.com. admin.c2yz.com. (
-                        2025102101 ; serial (update)
-                        3600       ; refresh
-                        600        ; retry
-                        604800     ; expire
-                        86400      ; minimum
-              )
-              @ IN NS ns1.c2yz.com.
-              ns1 IN A 192.168.200.1
-              @ IN A 192.168.200.1
-            '';
-          };
-        };
       };
     };
   };
